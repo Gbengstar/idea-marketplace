7a4282e110ac053f9e66e8e2e6c5048a5011ada3
gbenga oyewole <oyewolegbenga360@gmail.com>
2024-04-05 21:31:39 +0100

    fix: optimise wish list

--- src/wish-list/controller/wish-list.controller.ts
+++ src/wish-list/controller/wish-list.controller.ts
@@ -1,58 +1,70 @@
-import { Body, Controller, Get, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
 import { WishListService } from '../service/wish-list.service';
-import { StringValidationPipe } from '../../../libs/utils/src/pipe/validation.pipe';
+import {
+  ObjectValidationPipe,
+  StringValidationPipe,
+} from '../../../libs/utils/src/pipe/validation.pipe';
 import { stringValidator } from '../../../libs/utils/src/validator/custom.validator';
 import { TokenDecorator } from '../../../libs/utils/src/token/decorator/token.decorator';
 import { TokenDataDto } from '../../../libs/utils/src/token/dto/token.dto';
-import { Types } from 'mongoose';
+import { PipelineStage, Types } from 'mongoose';
+import { objectIdValidator } from '../../../libs/utils/src/validator/objectId.validator';
+import { PaginationDto } from '../../../libs/utils/src/pagination/dto/paginate.dto';
+import { paginationValidator } from '../../../libs/utils/src/pagination/validator/paginate.validator';
 
 @Controller('wish-list')
 export class WishListController {
   constructor(private readonly wishListService: WishListService) {}
 
-  @Post()
+  @Post('ads')
   async addToWishList(
     @Body('ads', new StringValidationPipe(stringValidator.required()))
     ads: string,
     @TokenDecorator() { id }: TokenDataDto,
   ) {
-    const wishList = await this.wishListService.findOneOrCreate(
-      { account: id },
-      { account: id },
-    );
-    const list = wishList.wishList.map((wishList) => wishList.toString());
-    wishList.wishList = [
-      ...new Set([ads, ...list]),
-    ] as unknown as Types.ObjectId[];
-    return wishList.save();
+    return this.wishListService.create({
+      account: id,
+      ref: 'ads',
+      wish: new Types.ObjectId(ads),
+    });
   }
 
-  @Get()
-  async getWishList(@TokenDecorator() { id }: TokenDataDto) {
-    const wishList = await this.wishListService.findOneOrCreate(
-      { account: id },
-      { account: id },
-    );
-
-    return wishList.populate([
-      { path: 'wishList', populate: { path: 'store' } },
-    ]);
-  }
-
-  @Patch()
-  async removeFromWishList(
-    @Body('ads', new StringValidationPipe(stringValidator.required()))
-    ads: string,
+  @Get('ads')
+  async getWishList(
     @TokenDecorator() { id }: TokenDataDto,
+    @Query(new ObjectValidationPipe(paginationValidator))
+    paginate: PaginationDto,
   ) {
-    const wishList = await this.wishListService.findOneOrCreate(
-      { account: id },
-      { account: id },
-    );
-    wishList.wishList = wishList.wishList.filter(
-      (wishList) => wishList.toString() !== ads,
-    );
+    const pipeline: PipelineStage[] = [
+      { $match: { account: new Types.ObjectId(id), ref: 'ads' } },
+      {
+        $lookup: {
+          from: 'ads',
+          foreignField: '_id',
+          as: 'ads',
+          localField: 'wish',
+          pipeline: [
+            {
+              $lookup: {
+                from: 'stores',
+                foreignField: '_id',
+                as: 'store',
+                localField: 'store',
+              },
+            },
+          ],
+        },
+      },
+    ];
+    return this.wishListService.aggregatePagination(paginate, pipeline);
+  }
 
-    return wishList.save();
+  @Delete('ads')
+  async deleteWishList(
+    @Query('id', new StringValidationPipe(objectIdValidator.required()))
+    id: string,
+    @TokenDecorator() { id: account }: TokenDataDto,
+  ) {
+    return this.wishListService.model.deleteOne({ account, _id: id });
   }
 }
--- src/wish-list/service/wish-list.service.ts
+++ src/wish-list/service/wish-list.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { BaseService } from '../../../libs/utils/src/database/service/db.service';
 import { WishList } from '../model/wish-list.model';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { FilterQuery, Model } from 'mongoose';
 
 @Injectable()
 export class WishListService extends BaseService<WishList> {
@@ -10,5 +10,12 @@ export class WishListService extends BaseService<WishList> {
     @InjectModel(WishList.name) private readonly WishListModel: Model<WishList>,
   ) {
     super(WishListModel);
+  }
+
+  async wishListIds(filter: FilterQuery<WishList>) {
+    const wishList = await this.find(filter);
+    if (!wishList[0]) return [];
+
+    return wishList.map((wish) => wish.wish.toString());
   }
 }
